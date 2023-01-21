e33f0c86edba437fffab37e1d578cecd9ed5020e
Kamal Preet Kaur <twinklehitech@gmail.com>
2023-01-22 00:09:08 +0530

    day4 done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function displayShow(images) {
     }, 3000);
   }
 
-let imageArr = ["https://img0.junaroad.com/images/banners/B2G1_13january2023_1673587401792.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1672290419954.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1674044135107.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1667988209100.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1652694665568.jpg?crsl_pos=0"]
+let imageArr = ["https://img0.junaroad.com/images/banners/B2G1_13january2023_1673587401792.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1667388470485.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1674029296684.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1672290419954.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1674044135107.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1667988209100.jpg?crsl_pos=0","https://img1.junaroad.com//assets/images/mobileNotif/img-1652694665568.jpg?crsl_pos=0"]
 
 window.addEventListener("load",function(){
     displayShow(imageArr);
